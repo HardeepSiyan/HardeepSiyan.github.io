06e2d2dac00f5cf14d2b36baa1f81278006f65da
Hardeep Singh <54856029+HardeepSiyan@users.noreply.github.com>
2021-07-29 20:46:16 -0400

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,8 @@
+function showAbout() {
+  var x = document.getElementById("AboutTab");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
